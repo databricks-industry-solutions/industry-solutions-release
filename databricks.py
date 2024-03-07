3561446e04607210f8c70448ec6ba97d3474a827
Antoine Amend <antoine.amend@gmail.com>
2024-03-07 13:38:34 -0500

    local sites

--- databricks.py
+++ databricks.py
@@ -17,37 +17,15 @@ root.addHandler(handler)
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", help="databricks path")
 parser.add_argument("-n", "--name", help="databricks solution code name, default to path basename")
-parser.add_argument("-d", "--deploy", help="deploy solution to S3", action='store_false')
-parser.add_argument("-v", "--vertical", help="industry vertical [fsi, rcpg, cme, hls, manuf]")
 
 args = parser.parse_args()
 
-if not args.path:
-    print("please provide a databricks path to download solution from")
-    sys.exit(1)
-
-if not args.name:
-    print("please provide a code name for this solution accelerator")
-    sys.exit(1)
-
-output_dir = f'site/{args.name}'
-if os.path.exists(output_dir):
-    shutil.rmtree(output_dir)
-os.makedirs(output_dir)
-
 accelerator = Accelerator(
     db_host=os.environ['DB_HOST'],
-    db_token=os.environ['DB_TOKEN'],
-    deploy=False
-    # aws_s3_bucket='databricks-web-files',
-    # aws_s3_path='notebooks/{solution_codename}/{file_name}',
-    # aws_s3_link='https://databricks-web-files.s3.us-east-2.amazonaws.com/notebooks',
-    # aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
-    # aws_secret_access_key=os.environ['AWS_ACCESS_SECRET']
+    db_token=os.environ['DB_TOKEN']
 )
 
 accelerator.release(
     db_path=args.path,
-    db_name=args.name,
-    output_dir=output_dir
+    db_name=args.name
 )
--- databricks/accelerator.py
+++ databricks/accelerator.py
@@ -1,5 +1,5 @@
 from databricks_api import DatabricksAPI
-import boto3
+import shutil
 import logging
 import base64
 from urllib.parse import quote, unquote
@@ -13,25 +13,27 @@ notebook_regex = re.compile(
     "DATABRICKS_NOTEBOOK_MODEL = '((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)'")
 
 
-def create_readme_page(solution_name, local_dir, content):
+def create_readme_page(solution_name, content):
     content_markdown = base64.b64decode(content['content']).decode('UTF-8')
     content_markdown = f'%md {content_markdown}'
     with open("content/template_readme.json", 'r') as f_in:
         json_object = json.loads(f_in.read())
         json_object['name'] = f'{solution_name} / README'
         json_object['commands'][0]['command'] = content_markdown
-        json_b64 = encode_notebook(json_object)
-        with open('content/template_readme.html', 'r') as file_in:
-            html_text = str(file_in.read())
-            notebook_html = html_text.replace('[README_CONTENT]', json_b64)
-            with open(f'{local_dir}/{solution_name}.html', "w") as file_out:
-                file_out.write(notebook_html)
+        return encode_json_to_notebook(json_object)
 
 
-def create_index_page(solution_name, local_dir, index_href):
-    content_notebook = '%md '
-    for notebook_href in index_href:
-        content_notebook = '{}\n\n{}'.format(content_notebook, notebook_href)
+def persist_readme_page(solution_name, local_dir, content):
+
+    with open('content/template_readme.html', 'r') as file_in:
+        html_text = str(file_in.read())
+        notebook_html = html_text.replace('[DATABRICKS_NOTEBOOK_MODEL]', content)
+
+    with open(f'{local_dir}/{solution_name}.html', "w") as file_out:
+        file_out.write(notebook_html)
+
+
+def create_index_page(solution_name, index_href):
 
     with open("content/template_index.json", 'r') as f_in:
         json_object = json.loads(f_in.read())
@@ -44,21 +46,21 @@ def create_index_page(solution_name, local_dir, index_href):
         json_object['commands'][0]['command'] = cmd
         json_str = json.dumps(json_object)
         json_str = quote(json_str).encode('utf-8')
-        json_b64 = base64.b64encode(json_str).decode('UTF-8')
-        with open('content/template_index.html', 'r') as file_in:
-            output_file = '{}/index.html'.format(local_dir)
-            with open(output_file, "w") as file_out:
-                html_text = str(file_in.read())
-                html_text = enrich_html(html_text)
-                html_text = html_text.replace('[NOTEBOOK_HTML_LINK]', f'{solution_name}.html')
-                html_text = html_text.replace('[NOTEBOOK_CONTENT]', json_b64)
-                html_text = html_text.replace('[SOLUTION_NAME]', solution_name)
-                file_out.write(html_text)
+        return base64.b64encode(json_str).decode('UTF-8')
 
 
-def encode_notebook(notebook_json):
-    notebook_encoded = quote(json.dumps(notebook_json))
-    return base64.b64encode(notebook_encoded.encode('utf-8')).decode('utf-8')
+def persist_index_page(solution_name, local_dir, index_notebook, landing_page):
+
+    with open('content/template_index.html', 'r') as f:
+        output_file = '{}/index.html'.format(local_dir)
+        html_text = str(f.read())
+        html_text = html_text.replace('[NOTEBOOK_HTML_LINK]', landing_page)
+        html_text = html_text.replace('[NOTEBOOK_CONTENT]', index_notebook)
+        html_text = html_text.replace('[SOLUTION_NAME]', solution_name)
+        html_text = enrich_html(html_text)
+
+    with open(output_file, "w") as f:
+        f.write(html_text)
 
 
 def get_section_name(section):
@@ -71,25 +73,33 @@ def get_section_name(section):
         return 'Context'
 
 
-def process_notebook_section(org_notebook, section_id, subsection_id, commands):
-    new_notebook = org_notebook
-    new_notebook['commands'] = commands
+def process_notebook_section(notebook_json, section_id, subsection_id, commands):
+    notebook_json['commands'] = commands
     notebook_name = get_section_name(commands[0])
-    notebook_encoded = encode_notebook(new_notebook)
+    notebook_encoded = encode_json_to_notebook(notebook_json)
     return Section(section_id, subsection_id, notebook_name, notebook_encoded)
 
 
-def process_notebook_content(notebook_id, notebook_content, notebook_name):
+def decode_notebook_to_json(notebook_content):
     org_notebook = base64.b64decode(notebook_content).decode('utf-8')
-    org_notebook = json.loads(unquote(org_notebook))
-    org_notebook['name'] = notebook_name
+    return json.loads(unquote(org_notebook))
+
+
+def encode_json_to_notebook(notebook_json):
+    notebook_encoded = quote(json.dumps(notebook_json))
+    return base64.b64encode(notebook_encoded.encode('utf-8')).decode('utf-8')
+
+
+def process_notebook_content(notebook_id, notebook_content, notebook_name):
+    notebook_json = decode_notebook_to_json(notebook_content)
+    notebook_json['name'] = notebook_name
 
     commands = []
     notebooks = []
     section_id = notebook_id + 1
     subsection_id = 0
 
-    for command in org_notebook['commands']:
+    for command in notebook_json['commands']:
 
         if not commands:
             commands = [command]
@@ -97,14 +107,14 @@ def process_notebook_content(notebook_id, notebook_content, notebook_name):
             command_value = command['command']
             # Start of a new top level section
             if head_section.match(command_value):
-                new_notebook = process_notebook_section(org_notebook, section_id, subsection_id, commands)
+                new_notebook = process_notebook_section(notebook_json, section_id, subsection_id, commands)
                 notebooks.append(new_notebook)
                 commands = [command]
                 subsection_id = 0
                 section_id += 1
             # Start of a second level section
             elif part_section.match(command_value):
-                new_notebook = process_notebook_section(org_notebook, section_id, subsection_id, commands)
+                new_notebook = process_notebook_section(notebook_json, section_id, subsection_id, commands)
                 notebooks.append(new_notebook)
                 commands = [command]
                 subsection_id += 1
@@ -112,7 +122,7 @@ def process_notebook_content(notebook_id, notebook_content, notebook_name):
                 # following of previous sections
                 commands.append(command)
 
-    new_notebook = process_notebook_section(org_notebook, section_id, subsection_id, commands)
+    new_notebook = process_notebook_section(notebook_json, section_id, subsection_id, commands)
     notebooks.append(new_notebook)
     return notebooks
 
@@ -126,11 +136,12 @@ def extract_content(html_content):
 
 
 def transform_html(org_html, notebook_encoded):
-    return re.sub(
+    notebook_encoded = re.sub(
         notebook_regex,
         "DATABRICKS_NOTEBOOK_MODEL = '{}'".format(notebook_encoded),
         org_html
     )
+    return notebook_encoded
 
 
 def valid_file(o):
@@ -142,16 +153,19 @@ def valid_file(o):
 
 
 def enrich_html(html_content):
-    html_tag_header = open("content/tag_header.html", "r").read()
-    html_tag_body = open("content/tag_body.html", "r").read()
-    html_tag_body_end = open("content/tag_body_end.html", "r").read()
-    html_content = html_content.replace('<head>', '<head>\n{}'.format(html_tag_header))
-    html_content = html_content.replace('<body>', '<body>\n{}'.format(html_tag_body))
-    html_content = html_content.replace('</body>', '{}\n</body>'.format(html_tag_body_end))
+    with open("content/tag_header.html", "r") as f:
+        t = f.read()
+        html_content = html_content.replace('<head>', '<head>\n{}'.format(t))
+    with open("content/tag_body.html", "r") as f:
+        t = f.read()
+        html_content = html_content.replace('<body>', '<body>\n{}'.format(t))
+    with open("content/tag_body_end.html", "r") as f:
+        t = f.read()
+        html_content = html_content.replace('</body>', '{}\n</body>'.format(t))
     return html_content
 
 
-def create_index(notebook_name, notebook_title):
+def create_index_html_element(notebook_name, notebook_title):
     return '<a class=index href="{}">{}</a>'.format(notebook_name, notebook_title)
 
 
@@ -162,97 +176,56 @@ class Section:
         self.notebook_name = notebook_name
         self.notebook_encoded = notebook_encoded
         self.logger = logging.getLogger('databricks')
-        self.logger.debug("{}/{} - {}".format(section_id, subsection_id, notebook_name))
 
     def html_name(self, solution_name):
-        return "{}_{}-{}.html".format(
-            solution_name,
-            self.section_id,
-            self.subsection_id
-        )
+        return f"{solution_name}_{self.section_id}-{self.subsection_id}.html"
 
     def get_number(self):
         if self.subsection_id == 0:
-            return "{}".format(self.section_id)
+            return f"{self.section_id}"
         else:
-            return "{}.{}".format(self.section_id, self.subsection_id)
+            return f"{self.section_id}.{self.subsection_id}"
 
 
 class Accelerator:
 
-    def __init__(
-            self,
-            db_host,
-            db_token,
-            deploy=False,
-            aws_s3_bucket=None,
-            aws_s3_path=None,
-            aws_s3_link=None,
-            aws_access_key_id=None,
-            aws_secret_access_key=None
-    ):
-
+    def __init__(self, db_host, db_token):
         self.logger = logging.getLogger('databricks')
-        self.deploy = deploy
-
-        # read databricks config
         self.db = DatabricksAPI(host=db_host, token=db_token)
 
-        # optionally - read S3 config
-        if deploy:
-
-            s3_conf = [x for x in [
-                aws_s3_bucket,
-                aws_s3_path,
-                aws_access_key_id,
-                aws_s3_link,
-                aws_secret_access_key
-            ] if not x]
-
-            if len(s3_conf) > 0:
-                raise Exception('Unable to deploy solution to S3, AWS configuration items are invalid')
-
-            aws = boto3.resource('s3',
-                                 aws_access_key_id=aws_access_key_id,
-                                 aws_secret_access_key=aws_secret_access_key
-                                 )
-            self.s3_path = aws_s3_path
-            self.s3_link = aws_s3_link
-            self.s3 = aws.Bucket(aws_s3_bucket)
-
     def export_to_html(self, remote_path, local_dir, solution_name):
+
         self.logger.info("Exporting solution accelerator to HTML file(s)")
-
-        objects = self.db.workspace.list(remote_path)['objects']
-        notebooks = [o['path'] for o in objects if valid_file(o)]
-        index = []
+        db_objects = self.db.workspace.list(remote_path)['objects']
+        db_notebooks = [db_object['path'] for db_object in db_objects if valid_file(db_object)]
+        index_html = []
+        landing_page = None
         section_id = 0
-        total_notebooks = len(notebooks)
 
-        if total_notebooks == 0:
+        if len(db_notebooks) == 0:
             raise Exception('Could not find any valid notebook in solution accelerator. Check naming convention')
 
         self.logger.info("Importing solution [README.md] file")
         readme_file = False
-        for o in objects:
-            if o['object_type'] == 'FILE' and o['path'].split('/')[-1] == 'README.md':
-                readme_content = self.db.workspace.export_workspace(o['path'])
-                create_readme_page(solution_name, local_dir, readme_content)
+        for db_object in db_objects:
+            if db_object['object_type'] == 'FILE' and db_object['path'].split('/')[-1] == 'README.md':
                 readme_file = True
-                index.append(create_index(f'{solution_name}.html', 'Context'))
+                readme_content = self.db.workspace.export_workspace(db_object['path'])
+                readme_notebook = create_readme_page(solution_name, readme_content)
+                persist_readme_page(solution_name, local_dir, readme_notebook)
+                landing_page = f'{solution_name}.html'
+                index_html.append(create_index_html_element(landing_page, 'Context'))
                 break
         if not readme_file:
-            raise Exception('Could not find [README.md] file')
+            logging.warning("Could not find README file in solution")
 
-        for i, file in enumerate(sorted(notebooks)):
+        for i, file in enumerate(sorted(db_notebooks)):
 
             html_content = self.db.workspace.export_workspace(file, format='HTML')
-            html_text = str(base64.b64decode(html_content['content']), 'utf-8')
-            notebook_name = '{} / {}'.format(
-                solution_name,
-                file.split('/')[-1]
-            )
-            self.logger.info("Processing notebook {}/{} [{}]".format(i + 1, total_notebooks, file.split('/')[-1]))
+            html_text = base64.b64decode(html_content['content']).decode('utf-8')
+            notebook_name = '{} / {}'.format(solution_name, file.split('/')[-1])
+
+            self.logger.info("Processing notebook {} [{}]".format(i + 1, file.split('/')[-1]))
             notebook = extract_content(html_text)
             children = process_notebook_content(section_id, notebook, notebook_name)
             for child in children:
@@ -263,35 +236,18 @@ class Accelerator:
                     child_html = transform_html(html_text, child.notebook_encoded)
                     f_out.write(child_html)
                     child_title = "{} {}".format(child.get_number(), child.notebook_name)
-                    index.append(create_index(child_name, child_title))
+                    index_html.append(create_index_html_element(child_name, child_title))
+                    if not landing_page:
+                        landing_page = child_name
 
         self.logger.info("Create Index page")
-        create_index_page(solution_name, local_dir, index)
+        index_notebook = create_index_page(solution_name, index_html)
+        persist_index_page(solution_name, local_dir, index_notebook, landing_page)
 
-    def deploy_s3(self, files, solution_codename):
-        for file in files:
-            file_name = os.path.basename(file)
-            remote_file = self.s3_path.format(
-                solution_codename=solution_codename,
-                file_name=file_name
-            )
-            self.logger.info(f"Publishing [{file_name}] to s3")
-            with open(file, 'r') as f:
-                data = f.read()
-                self.s3.put_object(
-                    Key=remote_file,
-                    Body=data,
-                    ACL='public-read',
-                    ContentType='text/html'
-                )
-
-    def release(self, db_path, db_name, output_dir):
+    def release(self, db_path, db_name):
+        output_dir = 'site'
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+        os.makedirs(output_dir)
         self.logger.info(f"Releasing solution [{db_name}]")
         self.export_to_html(db_path, output_dir, db_name)
-        if self.deploy:
-            files = os.listdir(output_dir)
-            self.deploy_s3([f"{db_path}/{file}" for file in files], db_name)
-            self.logger.info("Solution deployed to [{link}/{solution_codename}/index.html]".format(
-                link=self.s3_link,
-                solution_codename=db_name)
-            )
