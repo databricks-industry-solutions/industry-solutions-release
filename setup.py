d652d831dca60efc6cb3f00f69e0e11ce3e69bf5
Antoine Amend <antoine.amend@gmail.com>
2024-03-08 20:50:36 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-import os
-import configparser
-import io
-
-config = configparser.ConfigParser()
-config.read_file(io.StringIO('''[GOOGLE]
-host = https://6177827686947384.4.gcp.databricks.com/
-token = dapib1a4cb866568ab8dfaabc3512a5c9f70
-
-[DEMO]
-host = https://e2-demo-west.cloud.databricks.com/
-token = dapif843928f124620902fee9e81c5dd39a5
-
-[FIELD]
-host = https://e2-demo-field-eng.cloud.databricks.com/
-token = dapi26c802d8a3ea6bf3deea22cf4e194bbc'''))
-
-
-print(config['DEMO']['host'])
-print(config.sections())
-exit(0)
-
 from setuptools import find_packages, setup
 
 setup(
