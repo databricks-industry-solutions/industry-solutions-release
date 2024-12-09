72e66fd495581fa7d2c5eb19f4dd3ab16ae7a7e8
Aaron Zavora <aaron.zavora@databricks.com>
2024-12-09 15:06:32 -0500

    Update solutions.py

--- databricks/solutions.py
+++ databricks/solutions.py
@@ -217,7 +217,7 @@ class Accelerator:
         db_objects = list(self.db.workspace.list(self.db_path))
 
         # Retrieve list of numbered notebooks. Those will be our core story telling assets
-        db_notebooks = [[x.path for x in db_objects] for db_object in db_objects if valid_file(db_object)]
+        db_notebooks = [[x.path for x in db_objects] for db_object in db_objects if valid_file(db_object)][0]
 
         # Append list of numbered notebooks (story telling) with whatever additional util notebooks
         # Those would be added to the end of the index in alphabetical order
